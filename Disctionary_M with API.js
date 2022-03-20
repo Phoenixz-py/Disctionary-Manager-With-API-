7263468291e501eb5eb512f41376218b3a033b8a
Fenguin Phoenix <73833580+Phoenixz-py@users.noreply.github.com>
2022-03-20 21:52:52 +0530

    Update Disctionary_M with API.js

--- Disctionary_M with API.js
+++ Disctionary_M with API.js
@@ -1,3 +1,5 @@
+//without API one
+
 console.log('Welcome to the dictionary manager By Phoenix');
 let dictionary = ['test']
 
@@ -39,53 +41,50 @@ let b = () => {
             }        
 b();
 
-//Below is APi one last step for backend OPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
+//Below is APi one last step for backend
 
-// const express = require('express');
-// const app = express()
+const express = require('express');
+const app = express()
 
-// const prompt = require('prompt');
+const prompt = require('prompt');
 
-// const nemo = (nems) => {
-//   prompt.start()
-//   prompt
-//       .get({
-//           properties: {
-//               word: {
-//                   description: 'Press 1 to push word, Press 2 to search word in array',
-//               },
-//               hemlo: {
-//                   description: 'Enter',
-//               },    
-//           },
-//       })
-//        .catch((err) => {
-//           console.log('An error be noice ' + err);
-//     })
-// .then((res) => {
-// if(res['word'] == 1) {
-//  dictionary.push(res['hemlo'])
-//  console.log(('The word has been added: ') + res['word'])
-//   console.log(dictionary)  
-// } 
-//   else {
-//  if(dictionary.includes(res['hemlo'] ) ) {
+const nemo = (nems) => {
+  prompt.start()
+  prompt
+      .get({
+          properties: {
+              word: {
+                  description: 'Press 1 to push word, Press 2 to search word in array',
+              },
+              hemlo: {
+                  description: 'Enter',
+              },    
+          },
+      })
+       .catch((err) => {
+          console.log('An error be noice ' + err);
+    })
+.then((res) => {
+if(res['word'] == 1) {
+ dictionary.push(res['hemlo'])
+ console.log(('The word has been added: ') + res['word'])
+  console.log(dictionary)  
+} 
+  else {
+ if(dictionary.includes(res['hemlo'] ) ) {
                                   
-//  console.log('The word has been found');
-//   }
-//   else {
-//    console.log('Word has not been found')
-// }
-//   }
-// })
-//           }
-// //need adjustments so front just call it on wish
-// app.get('/nemo', (req, res) => { res.send(nemo())
-//  }
-// );
+ console.log('The word has been found');
+  }
+  else {
+   console.log('Word has not been found')
+}
+  }
+})
+  }
+ app.get('/nemo', (req, res) => { res.send(nemo())
+ }
+);
 
 
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => console.log(`listening to ${port}...`))
-
-//and it works
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening to ${port}...`))
